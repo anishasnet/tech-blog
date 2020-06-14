39067fc7700740439968886bfd001e7a78762e1d
anishasnet <anishasnet@gmail.com>
2020-06-13 22:55:08 -0700

    Created associations between Users, Posts, Comments

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post);
+
+User.hasMany(Comment);
+
+Post.belongsTo(User);
+
+Post.hasMany(Comment)
+
+Comment.belongsTo(User);
+
+Comment.belongsTo(Post);
+
+module.exports = { User, Post, Comment };
