8368a0d3be1e8910e8ab471a3bfab9598d455c63
anishasnet <anishasnet@gmail.com>
2020-06-14 23:43:42 -0700

    Complete handlebars added and javascript

--- models/User.js
+++ models/User.js
@@ -1,7 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt')
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPW) {
+    return bcrypt.compareSync(loginPW, this.password);
+  }
+}
+
 User.init(
     {
       id: {
@@ -24,7 +30,18 @@ User.init(
         }
     },
     {
-    sequelize,
+        hooks: {
+          async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+
+          async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          }
+        },
+        sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
