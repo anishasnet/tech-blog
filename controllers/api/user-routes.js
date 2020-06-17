e5e0d0f809d76419878798962c055abdd2b9f948
anishasnet <anishasnet@gmail.com>
2020-06-16 23:37:52 -0700

    Cleaned up user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -74,46 +74,6 @@ router.post('/', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
-    User.findOne({
-        where: {
-            username: req.body.username
-        }
-    }).then(dbUserData => {
-        if (!dbUserData) {
-            res.status(400).json({ message: 'That username does not exist.'});
-            return;
-        }
-        const validPassword = dbUserData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res.status(400).json({ message: 'The password entered is incorrect.' });
-            return;
-        }
-        req.session.save(() => {
-            req.session.user_id = dbUserData.id;
-            req.session.username = dbUserData.username;
-            req.session.loggedIn = true;
-      
-            res.json({ user: dbUserData, message: 'You are logged in.' });
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
-
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
-});
-
 router.put('/:id', (req, res) => {
     User.update(req.body, {
         individualHooks: true,
